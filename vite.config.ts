83477c3d7471d99d1293a3b26d7ff17d77777ef5
‘lianyutian’ <1436159527@qq.com>
2024-01-05 17:35:19 +0800

    修改接口环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 //引入svg需要用到插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 //mock插件提供方法
 import { viteMockServe } from 'vite-plugin-mock'
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  //获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd())
   return {
     plugins: [
       vue(),
@@ -31,6 +33,19 @@ export default defineConfig(({ command }) => {
         scss: {
           javascriptEnabled: true,
           additionalData: '@import "./src/styles/variable.scss";',
+        },
+      },
+    },
+    //代理跨域
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          //获取数据服务器地址的设置
+          target: env.VITE_SERVE,
+          //需要代理跨域
+          changeOrigin: true,
+          //路径重写
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
