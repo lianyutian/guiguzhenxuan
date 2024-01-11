d09c4b29036e7e3e748aa7760b283d2cf51fd276
‘lianyutian’ <1436159527@qq.com>
2024-01-11 18:02:15 +0800

    feat: spu管理

---
+++ src/api/product/spu/index.ts
@@ -0,0 +1,62 @@
+//SPU管理模块的接口
+import request from '@/utils/request'
+import type { HasSpuResponseData } from './type'
+enum API {
+  //获取已有的SPU的数据
+  HASSPU_URL = '/admin/product/',
+  //获取全部品牌的数据
+  ALLTRADEMARK_URL = '/admin/product/baseTrademark/getTrademarkList',
+  //获取某个SPU下的全部的售卖商品的图片数据
+  IMAGE_URL = '/admin/product/spuImageList/',
+  //获取整个项目全部的销售属性[颜色、版本、尺码]
+  ALLSALEATTR_URL = '/admin/product/baseSaleAttrList',
+  //获取某一个SPU下全部的已有的销售属性接口地址
+  SPUHASSALEATTR_URL = '/admin/product/spuSaleAttrList/',
+  //追加一个新的SPU
+  ADDSPU_URL = '/admin/product/saveSpuInfo',
+  //更新已有的SPU
+  UPDATESPU_URL = '/admin/product/updateSpuInfo',
+}
+
+//获取某一个三级分类下已有的SPU数据
+export const reqHasSpu = (
+  page: number,
+  limit: number,
+  category3Id: string | number,
+) => {
+  return request.get<any, HasSpuResponseData>(
+    API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`,
+  )
+}
+
+//获取全部的SPU的品牌的数据
+export const reqAllTradeMark = () => {
+  return request.get<any, AllTradeMark>(API.ALLTRADEMARK_URL)
+}
+
+//获取某一个已有的SPU下全部商品的图片地址
+export const reqSpuImageList = (spuId: number) => {
+  return request.get<any, SpuHasImg>(API.IMAGE_URL + spuId)
+}
+
+//获取全部的销售属性
+export const reqAllSaleAttr = () => {
+  return request.get<any, HasSaleAttrResponseData>(API.ALLSALEATTR_URL)
+}
+
+//获取某一个已有的SPU拥有多少个销售属性
+export const reqSpuHasSaleAttr = (spuId: number) => {
+  return request.get<any, SaleAttrResponseData>(API.SPUHASSALEATTR_URL + spuId)
+}
+
+//添加一个新的SPU的
+//更新已有的SPU接口
+//data:即为新增的SPU|或者已有的SPU对象
+export const reqAddOrUpdateSpu = (data: any) => {
+  //如果SPU对象拥有ID,更新已有的SPU
+  if (data.id) {
+    return request.post<any, any>(API.UPDATESPU_URL, data)
+  } else {
+    return request.post<any, any>(API.ADDSPU_URL, data)
+  }
+}
--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,4 +1,132 @@
-<template>spu</template>
+<template>
+  <div>
+    <!-- 三级分类 -->
+    <Category :scene="scene"></Category>
+    <!-- SPU展示页面 -->
+    <div v-show="scene === 0">
+      <el-card style="margin: 10px 0px">
+        <el-button
+          type="primary"
+          size="default"
+          icon="Plus"
+          :disabled="categoryStore.c3Id ? false : true"
+          @click="addSpu"
+        >
+          添加SPU
+        </el-button>
+        <el-table border style="margin: 10px 0px" :data="records">
+          <el-table-column
+            label="序号"
+            type="index"
+            align="center"
+            width="80px"
+          ></el-table-column>
+          <el-table-column label="SPU名称" prop="spuName"></el-table-column>
+          <el-table-column label="SPU描述" prop="description"></el-table-column>
+          <el-table-column label="SPU操作">
+            <!-- row 回传编辑数据 -->
+            <template #default="{ row }">
+              <el-button
+                type="primary"
+                size="small"
+                icon="Edit"
+                @click="updateSpu(row)"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </div>
+    <!-- SPU编辑页面 -->
+    <SpuForm
+      v-show="scene === 1"
+      ref="spu"
+      @changeScene="changeScene"
+    ></SpuForm>
+    <!-- 分页器 -->
+    <el-pagination
+      v-model:current-page="pageNo"
+      v-model:page-size="pageSize"
+      :page-sizes="[3, 5, 7, 9]"
+      :background="true"
+      layout="prev, pager, next, jumper,->, sizes,total"
+      :total="total"
+      @current-change="getHasSpu"
+      @size-change="changeSize"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import useCategoryStore from '@/store/modules/category'
+import { reqHasSpu } from '@/api/product/spu'
+import { HasSpuResponseData, SpuData } from '@/api/product/spu/type'
+import SpuForm from './spuForm.vue'
 
-<script setup lang="ts"></script>
+//分类仓库数据
+const categoryStore = useCategoryStore()
+//场景的数据
+let scene = ref<number>(0)
+//分页器默认页码
+let pageNo = ref<number>(1)
+//每一页展示几条数据
+let pageSize = ref<number>(3)
+// 存储已有品牌数据总数
+let total = ref<number>(0)
+// 存储已有SPU的数据
+let records = ref<any>([])
+//此方法执行:可以获取某一个三级分类下全部的已有的SPU
+const getHasSpu = async (pager = 1) => {
+  console.log(pager)
+
+  //修改当前页码
+  pageNo.value = pager
+  let result: HasSpuResponseData = await reqHasSpu(
+    pageNo.value,
+    pageSize.value,
+    categoryStore.c3Id,
+  )
+  if (result.code == 200) {
+    records.value = result.data.records
+    total.value = result.data.total
+  }
+}
+// 监听c3Id变化
+watch(
+  () => categoryStore.c3Id,
+  () => {
+    //清空上一次查询的SPU
+    records.value = []
+    //保证三级分类得有才能发请求
+    if (!categoryStore.c3Id) return
+    //获取SPU
+    getHasSpu()
+  },
+)
+//分页器下拉菜单发生变化的时候触发
+const changeSize = () => {
+  getHasSpu()
+}
+
+//添加新的SPU按钮的回调
+const addSpu = () => {
+  //切换为场景1:添加与修改已有SPU结构->SpuForm
+  scene.value = 1
+}
+//获取子组件实例
+const spu = ref<any>()
+//修改已有的SPU的按钮的回调
+const updateSpu = (row: SpuData) => {
+  //切换为场景1:添加与修改已有SPU结构->SpuForm
+  scene.value = 1
+  //调用子组件实例方法获取完整已有的SPU的数据
+  console.log(spu.value)
+
+  spu.value.initHasSpuData(row)
+}
+
+const changeScene = () => {
+  scene.value = 0
+}
+</script>
 <style lang="scss" scoped></style>
